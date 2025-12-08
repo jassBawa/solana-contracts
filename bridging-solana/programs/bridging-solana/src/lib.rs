b783015a09196c110062b251dcd4b123e1d2bcd9
jassbawa <jasspreetsbawa@gmail.com>
2025-12-08 16:47:36 +0530

    init: methods

---
+++ bridging-solana/programs/bridging-solana/src/lib.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+
+declare_id!("AgoXqc3Kt3a1ckqHEvWw9n9KBYBzozcozQDsG3zRDX5m");
+
+mod errors;
+mod instructions;
+mod state;
+
+use instructions::*;
+
+#[program]
+pub mod bridging_solana {
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<InitializeBridge>,
+        destination_chain_id: u64,
+        destination_bridge: [u8; 20],
+        relayer: Pubkey,
+    ) -> Result<()> {
+        instructions::initialize_bridge(ctx, destination_chain_id, destination_bridge, relayer)
+    }
+
+    pub fn lock_tokens(
+        ctx: Context<LockTokens>,
+        amount: u64,
+        destination_address: [u8; 20],
+    ) -> Result<()> {
+        instructions::lock_tokens(ctx, amount, destination_address)
+    }
+}
